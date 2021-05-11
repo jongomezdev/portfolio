30d01932afcdff7d49cd877d4090acf67596c410
Jonathan Gomez <jongomezdev@gmail.com>
2021-05-11 11:15:58 -0400

    remove comments

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,46 +1,7 @@
 import React from 'react';
-// import styled from 'styled-components';
-
-// const NavStyles = styled.div`
-//   a {
-//     display: inline-block;
-//     position: relative;
-//     color: #000;
-//     text-decoration: none;
-//   }
-//   a:hover {
-//     color: #000;
-//   }
-//   a::before {
-//     content: '';
-//     position: absolute;
-//     width: 100%;
-//     height: 2px;
-//     bottom: 0;
-//     left: 0;
-//     background-color: #000;
-//     visibility: hidden;
-//     transform: scaleX(0);
-//     transition: all 0.3s ease-in-out;
-//   }
-//   a:hover::before {
-//     visibility: visible;
-//     transform: scaleX(1);
-//   }
-//   .github {
-//     font-size: 1.5rem;
-//   }
-
-//   @media (max-width: 800px) {
-//     .word {
-//       display: none;
-//     }
-//   }
-// `;
 
 export default function Navbar() {
   return (
-    // <NavStyles>
     <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top fixed-top shadow-lg p-3 mb-5 bg-white rounded alert container">
       <a className="navbar-brand" title="Jonathan's Portfolio" href="/">
         Jonathan Gomez
@@ -103,6 +64,5 @@ export default function Navbar() {
         </span>
       </div>
     </nav>
-    // </NavStyles>
   );
 }
